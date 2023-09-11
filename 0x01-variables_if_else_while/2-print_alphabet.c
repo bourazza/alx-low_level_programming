e1366597ec0b65d33570a1fb0083c9d18cb0a2c2
bourazza <zakinirvana482@example.com>
2023-09-11 16:43:23 -0700

    print alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,40 +1,17 @@
 #include <stdio.h>
 
 /**
- * main - prints out all possible combos of 3 digits
- * Return: 0
+ * main - prints the alphabet in lowercase,
+ * followed by a new line
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
+	char ch;
 
-	int i, j, k;
-
-	for (i = 48; i < 58; i++)
+	for (ch = 'a'; ch <= 'z'; ch++)
 	{
-		for (j = i; j < 58; j++)
-		{
-			for (k = j; k < 58; k++)
-			{
-				if (i == j || j == k || i == k)
-				{
-					continue;
-				}
-				putchar(i);
-				putchar(j);
-				putchar(k);
-
-				if (i == 55 && j == 56 && k == 57)
-				{
-					break;
-				}
-				else
-				{
-					putchar(',');
-					putchar(' ');
-				}
-
-			}
-		}
+		putchar(ch);
 	}
 	putchar('\n');
 	return (0);
